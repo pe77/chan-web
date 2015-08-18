fa8fec7fcf349b69f969ebf833bfd234023e1abb
P <p321654987@outlook.com>
2015-08-18 18:10:45 -0300

    ajuste layout + marcação de id das respostas

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
   <link href="css/vendor/all.css" rel="stylesheet">
   <link href="css/vendor/angular-carousel.css" rel="stylesheet">
   <link href="css/app/app.css" rel="stylesheet">
-  <link href="css/chan.css" rel="stylesheet">
+  <link href="css/chan.css?v1.2" rel="stylesheet">
   <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -112,7 +112,7 @@
         <a href="#" data-toggle="sidebar-menu" class="toggle pull-left visible-xs">
           <i class="fa fa-bars"></i>
         </a>
-        <a class="navbar-brand" href="index.html">77Chan</a>
+        <a class="navbar-brand" href="#">77Chan</a>
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
           <span class="sr-only">Toggle navigation</span>
           <span class="icon-bar"></span>
