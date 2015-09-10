606221a6369292ffa8e5a03c4202cb4a16884fd6
P <p321654987@outlook.com>
2015-09-10 11:53:15 -0300

    upd config sample

--- app/config.sample.js
+++ app/config.sample.js
@@ -1,8 +1,9 @@
 var parameters = {
 	api_url : "[API_URL]",
 	google_api_key : "[API_KEY]",
+	facebook_api_key : "[API_KEY]",
 	file_path : "[IMAGE_PATH]",
 	messages_update_interval : 60000, // 1 minuto
 	page_limit : 15, // 15 posts por pagina
-	auto_update_time: 10, // 10 segundos
+	auto_update_time: 1000*15, // 10 segundos
 };
